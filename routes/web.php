27b570fc357a287e6bb7975aa6b84bb5b644f44d
Patryk <patryk.r.nowak@gmail.com>
2023-02-21 07:03:28 +0000

    admin - general set

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
