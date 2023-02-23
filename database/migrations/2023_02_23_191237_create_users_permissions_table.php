407622671805668cd04101e517a430027ffe2040
Patryk <patryk.r.nowak@gmail.com>
2023-02-23 20:19:00 +0000

    added migration - roles + permissions

---
+++ database/migrations/2023_02_23_191237_create_users_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+// pivot table
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions_user', function (Blueprint $table) {
+            // $table->id();
+            $table->primary(['user_id', 'permission_id']);
+            $table->foreignId("user_id")->constrained()->onDelete('cascade');
+            $table->foreignId("permission_id")->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions_user');
+    }
+};
